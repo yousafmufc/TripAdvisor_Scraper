26cfa7c6955dc59c0e7a8142ceb5136f1bbeaa38
caught_offside <33321743+yousafmufc@users.noreply.github.com>
2022-06-22 20:40:30 +0000

    Final code.

--- trip_advisor.py
+++ trip_advisor.py
@@ -1,3 +1,5 @@
+
+
 from selenium import webdriver
 #in replit environment, we have to import options for selenium
 from selenium.webdriver.chrome.options import Options
@@ -6,7 +8,9 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import time
 
-TRIP_ADVISOR_URL = 'https://www.tripadvisor.com/Attractions-g189952-Activities-a_allAttractions.true-Iceland.html'
+#visiting Iceland, Paris, Switzerland, and Rome
+PLACES_TO_VIST_URLS= ['https://www.tripadvisor.com/Attractions-g189952-Activities-a_allAttractions.true-Iceland.html','https://www.tripadvisor.com/Attractions-g187147-Activities-a_allAttractions.true-Paris_Ile_de_France.html','https://www.tripadvisor.com/Attractions-g188045-Activities-a_allAttractions.true-Switzerland.html','https://www.tripadvisor.com/Attractions-g187791-Activities-a_allAttractions.true-Rome_Lazio.html']
+PLACES = ['Iceland', 'Paris', 'Switzerland', 'Rome'] #place names
 
 
 
@@ -19,17 +23,17 @@ def get_driver():
   driver = webdriver.Chrome(options=chrome_options)
   return driver
 
-def get_things_to_do(driver):
+def get_things_to_do(driver,URL):
   #having driver import the page URL
-  driver.get(TRIP_ADVISOR_URL)
-  #creating a list of the top 15 things to do
+  driver.get(URL)
+  #creating a list of the top 30 things to do
   thingsToDo = driver.find_elements(By.XPATH,".//section[@data-automation='WebPresentation_SingleFlexCardSection']")
   return thingsToDo
 
 def get_top_hotels(driver):
   pass
 
-def parse_thingsToDo(thingsToDo):
+def parse_thingsToDo(thingsToDo,country):
   thumbnail_url = thingsToDo.find_element(By.XPATH,'.//div/span/div/article/div[1]/div/div/div/div[1]/a').find_element(By.TAG_NAME,'img').get_attribute('src')
   #print(thumbnail_url) #remove later
 
@@ -50,6 +54,7 @@ def parse_thingsToDo(thingsToDo):
   #print(link_to_user_reviews) #delete later
 
   return {
+    'Country': country,
     'Thing_to_do': thing_name,
     'Total_Reviews': total_reviews,
     'Average Rating': average_rating[:-8], #removing last few characters to remove "bubbles" word
@@ -60,16 +65,27 @@ def parse_thingsToDo(thingsToDo):
   }
   
 if __name__ == "__main__":
-  print('Creating driver')
-  driver = get_driver()
 
-  print('Fetching top things to do')
-  thingsToDo = get_things_to_do(driver)
+  i=0 #counter to determine whether we are writing data for first time or appending.
+  #this counter will also keep track of place name
+  
+  for place_URL in PLACES_TO_VIST_URLS:
+    print('Creating driver')
+    driver = get_driver()
+  
+    print('Fetching top things to do in {}.'.format(PLACES[i]))
+    thingsToDo = get_things_to_do(driver,place_URL)
+   
 
-  print('Parsing top 20 things to do')
-  top20_things = [parse_thingsToDo(thing) for thing in thingsToDo[:20]]
+    print('Parsing top 30 things to do in {}.'.format(PLACES[i]))
+    top30_things = [parse_thingsToDo(thing,PLACES[i]) for thing in thingsToDo[:30]]
+    
 
-  print('Save the top things to a CSV')
-  things_df = pd.DataFrame(top20_things) #things Pandas table
-
-  things_df.to_csv('topAttractions.csv',index=None)
+    print('Save the top things to a CSV')
+    things_df = pd.DataFrame(top30_things) #things Pandas table
+    #if statement below determines whether we are writing to CSV file for first time or appending.
+    if i==0:
+      things_df.to_csv('topAttractions.csv',index=None)
+    else:
+      things_df.to_csv('topAttractions.csv', mode='a', index=None, header=False)
+    i=i+1 #increment counter
