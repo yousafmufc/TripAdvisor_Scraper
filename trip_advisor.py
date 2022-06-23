f2eab434483be5839a0ad06255abe81afce2a426
caught_offside <33321743+yousafmufc@users.noreply.github.com>
2022-06-23 22:55:05 +0000

    final code. 6/23/22

--- trip_advisor.py
+++ trip_advisor.py
@@ -30,28 +30,25 @@ def get_things_to_do(driver,URL):
   thingsToDo = driver.find_elements(By.XPATH,".//section[@data-automation='WebPresentation_SingleFlexCardSection']")
   return thingsToDo
 
-def get_top_hotels(driver):
-  pass
 
 def parse_thingsToDo(thingsToDo,country):
   thumbnail_url = thingsToDo.find_element(By.XPATH,'.//div/span/div/article/div[1]/div/div/div/div[1]/a').find_element(By.TAG_NAME,'img').get_attribute('src')
-  #print(thumbnail_url) #remove later
-
+  
   thing_url = thingsToDo.find_element(By.XPATH,'.//div/span/div/article/div[2]/header/div/div/a[1]').get_attribute('href')
-  #print(thing_url) #remove later
+  
   thing_name = thingsToDo.find_element(By.XPATH,'.//div/span/div/article/div[2]/header/div/div/a[1]').text
-  #print(thing_name) #remove later
+  
 
   average_rating = thingsToDo.find_element(By.XPATH,'.//div/span/div/article/div[2]/header/div/div/a[2]').find_element(By.TAG_NAME,'svg').get_attribute('aria-label')
-  #print(average_rating) #remove later
+  
   total_reviews = thingsToDo.find_element(By.XPATH,'.//div/span/div/article/div[2]/header/div/div/a[2]').find_element(By.TAG_NAME,'span').text
-  #print(total_reviews) #delete later
+  
 
   thing_type = thingsToDo.find_element(By.XPATH,'.//div/span/div/article/div[2]/div[1]/div[1]/div/div/div').text
-  #print(thing_type) #delete later
+  
 
   link_to_user_reviews = thingsToDo.find_element(By.XPATH,'.//div/span/div/article/div[2]/div[1]/div[2]').find_element(By.TAG_NAME,'a').get_attribute('href')
-  #print(link_to_user_reviews) #delete later
+  
 
   return {
     'Country': country,
@@ -69,13 +66,12 @@ if __name__ == "__main__":
   i=0 #counter to determine whether we are writing data for first time or appending.
   #this counter will also keep track of place name
   
-  for place_URL in PLACES_TO_VIST_URLS:
+  for place_URL in PLACES_TO_VIST_URLS: #for loop to loop through each location visited
     print('Creating driver')
     driver = get_driver()
   
     print('Fetching top things to do in {}.'.format(PLACES[i]))
     thingsToDo = get_things_to_do(driver,place_URL)
-   
 
     print('Parsing top 30 things to do in {}.'.format(PLACES[i]))
     top30_things = [parse_thingsToDo(thing,PLACES[i]) for thing in thingsToDo[:30]]
@@ -83,9 +79,11 @@ if __name__ == "__main__":
 
     print('Save the top things to a CSV')
     things_df = pd.DataFrame(top30_things) #things Pandas table
+    
     #if statement below determines whether we are writing to CSV file for first time or appending.
     if i==0:
       things_df.to_csv('topAttractions.csv',index=None)
     else:
       things_df.to_csv('topAttractions.csv', mode='a', index=None, header=False)
     i=i+1 #increment counter
+  
